36db1daa4869a13f3a437c8f14b608dd5d7c2ab0
taha <momad6955@gmail.com>
2026-07-10 17:51:17 +0330

    Update app.py
    Test

--- app.py
+++ app.py
@@ -1105,4 +1105,6 @@ def export_excel():
                      as_attachment=True, download_name='Trip_Expenses.xlsx')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
